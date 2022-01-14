e089947d06b18174fd3476b6828858397cf0790e
Gaston Sanchez <sanchez-17@outlook.com>
2022-01-13 23:44:58 -0300

    Headless browsers

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -69,9 +69,15 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+Browser.site = 'https://boilerplate-mochachai-1.sanchez-17.repl.co'; // Your URL here
+
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
+  suiteSetup(function(done){
+    return browser.visit("/",done);
+  })
 
 
 
@@ -84,14 +90,26 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill("surname", "Colombo").then( () => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name","Cristoforo");
+          browser.assert.text('span#surname','Colombo');
+          browser.assert.elements('span#dates',1) 
+        })
+      })
       done();
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill('surname','Vespucci').then( ()=> {
+        browser.pressButton('submit', ()=> {
+          browser.assert.success();
+          browser.assert.text('span#name','Amerigo');
+          browser.assert.text('span#surname','Vespucci');
+          browser.assert.elements('span#dates',1)
+        })
+      })
       done();
     });
   });
